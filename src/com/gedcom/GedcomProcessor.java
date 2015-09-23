a57a22e1db9aac509366f716c52c112399b4f9f8
aditi <apate87@stevens.edu>
2015-09-23 12:43:21 -0400

    commiting latest changes - logic implemented for printing data in order

--- src/com/gedcom/GedcomProcessor.java
+++ src/com/gedcom/GedcomProcessor.java
@@ -1,72 +1,178 @@
 package com.gedcom;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
+class Individual {
+	String indi = "";
+	String name = "";
+	String sex = "";
+	String famc = "";
+	String fams = "";
+	
+	// birthdate and death date will add later
+	//constructor for Individual class
+	public Individual(String indi,String name,String sex, String famc, String fams)
+	{
+	this.indi = indi;
+	this.name = name;
+	this.sex = sex;
+	this.famc = famc;
+	this.fams = fams;
+	}
+}
 
-/**
- * This program reads a GEDCOM file , prints each line , level number of each line
- * and prints tha tag of each line that has a valid tagfor our project or prints "invalid tag"
- * @author aditi patel
- * @date 09/16/2015
- *
- */
+class Family 
+{
+	String famId = "";
+	String husbId = "";
+	String wifeId = "";
+	ArrayList<String> childIds = new ArrayList<String>();
+	
+	public Family(String famId, String husbId, String wifeId, ArrayList<String> childIds) {
+		this.famId = famId;
+		this.husbId = husbId;
+		this.wifeId = wifeId;
+		this.childIds = childIds;
+	}
+	
+}
+
 public class GedcomProcessor {
-
+	
 	public static void main(String[] args){
 
-		JFileChooser chooser = new JFileChooser();
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("GEDCOM files only" , "ged");
-		chooser.setFileFilter(filter);
-		int returnValue = chooser.showOpenDialog(chooser);
+		String indi = "";
+		String name = "";
+		String sex = "";
+		String famc = "";
+		String fams = "";
+		
+		String famId = "";
+		String husbId = "";
+		String wifeId = "";
+		ArrayList<String> childIds = new ArrayList<String>();
+		
+        ArrayList<String> validTags = new ArrayList<String>();
+		
+        validTags.add("INDI");
+		validTags.add("NAME"); 
+		validTags.add("SEX");
+		validTags.add("BIRT");
+		validTags.add("DEAT");
+		validTags.add("FAMC");
+		validTags.add("FAMS");
+		validTags.add("FAM");
+		validTags.add("MARR");
+		validTags.add("HUSB");
+		validTags.add("WIFE");
+		validTags.add("CHIL");
+		validTags.add("DIV");
+		validTags.add("DATE");
+		validTags.add("HEAD");
+		validTags.add("TRLR");
+		validTags.add("NOTE");
+		
+		Boolean indiHasDataBool = false;
+		Boolean familyHasDataBool = false;
+		
+		Map < String, Individual > individualMap = new HashMap <> (); 
+		Map < String, Family > familyMap = new HashMap <> (); 
+		ArrayList<String> individualIdList = new ArrayList<>();
+		ArrayList<String> familyList = new ArrayList<>();
 
-		if(returnValue == JFileChooser.APPROVE_OPTION){
-			java.io.File selectedFile = chooser.getSelectedFile();
-
-			ArrayList<String> validTags = new ArrayList<String>();
-			validTags.add("INDI");
-			validTags.add("NAME"); 
-			validTags.add("SEX");
-			validTags.add("BIRT");
-			validTags.add("DEAT");
-			validTags.add("FAMC");
-			validTags.add("FAMS");
-			validTags.add("FAM");
-			validTags.add("MARR");
-			validTags.add("HUSB");
-			validTags.add("WIFE");
-			validTags.add("CHIL");
-			validTags.add("DIV");
-			validTags.add("DATE");
-			validTags.add("HEAD");
-			validTags.add("TRLR");
-			validTags.add("NOTE");
-
-			Scanner input;
-
-			try{
-				input = new Scanner(selectedFile);
-				while(input.hasNextLine()){
-					String line = input.nextLine();
-					String inputElements[] = line.split(" ");
-					System.out.println(line); 
-					System.out.println("Level number:"+ inputElements[0]);
-					if(validTags.contains(inputElements[1])){
-						System.out.println("Valid tag:" + inputElements[1]);
-					}
-					else
-						System.out.println("Invalid tag");
-					System.out.println( );
+		try {
+			FileReader file = new FileReader(new File("Test2.ged"));
+			
+		
+	        Scanner inputFileData = new Scanner(file);
+	        
+	       while(inputFileData.hasNextLine()) {				
+				String line = inputFileData.nextLine();
+				String inputElements[] = line.split(" "); 
+				if(validTags.contains(inputElements[1])) {
+					switch(inputElements[1]) {
+						case "INDI" :
+							if(indiHasDataBool == true) {
+								Individual  member = new Individual(indi, name, sex, fams, famc);
+								individualMap.put(indi, member);
+								individualIdList.add(indi);
+								indi = "";
+								name = "";
+								sex = "";
+								famc = "";
+								fams = "";
+								indiHasDataBool = false;
+							}
+							indi = inputElements[2];
+							indiHasDataBool = true;
+							break;
+						case "NAME" :
+							name = inputElements[2];
+							break;
+						case "SEX" :
+							sex = inputElements[2];
+							//dataSetBool = true;
+							break;
+						case "FAMS" :
+							fams = inputElements[2];							
+							break;
+						case "FAMC" :
+							famc = inputElements[2];							
+							break;
+						case "FAM" :
+							if(familyHasDataBool == true) {
+								Family  family = new Family(famId, husbId, wifeId, childIds);
+								familyMap.put(famId, family);
+								familyList.add(famId);
+								famId = "";
+								husbId = "";
+								wifeId = "";
+								childIds = new ArrayList<String>();
+								familyHasDataBool = false;
+							}
+							famId = inputElements[2];
+							familyHasDataBool = true;
+							break;
+						case "HUSB" :
+							husbId = inputElements[2];							
+							break;
+						case "WIFE" :
+							wifeId = inputElements[2];
+							break;
+						case "CHIL" :
+							childIds.add(inputElements[2]);							
+							break;							
+					}					
 				}
-
+				else {
+				}					
 			}
-
-			catch(FileNotFoundException e){
-				e.printStackTrace();
-			}
+			
+	       
+	       for(String itr: individualIdList){
+	    	   Individual i = individualMap.get(itr);
+	    	 
+	    	   System.out.println("Individual ID : " + itr + "\n"+"Name : " + i.name);
+	    	   System.out.println();
+	       }
+	       
+	       for(String itr: familyList){
+	    	   Family f = familyMap.get(itr);
+	  System.out.println("Family Id : "+ itr +"\n"+"Husband Id : " + f.husbId +"\n"+ "Husband's name " + individualMap.get(f.husbId).name +
+			  "\n"+"Wifes Id : " + f.wifeId +"\n"+"Wifes Name : "+individualMap.get(f.wifeId).name);
+	  System.out.println();
+	       }
+	       
+			inputFileData.close();
 		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}		
 	}//end of main 
-}//end of class file
+}
